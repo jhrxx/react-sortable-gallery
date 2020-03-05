41eeacd997f1ab251ed6a43afd4b05a5c546df05
jhrxx <jhrxx@126.com>
2020-03-05 16:00:31 +0800

    init proj

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
+# react sortable gallery 
+[![Badge](https://img.shields.io/badge/link-996.icu-%23FF4D5B.svg)](https://996.icu/#/en_US)
+[![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
+
+
+**react sortable galler** 一个基于 [react](https://reactjs.org/) 支持拖拽排序的组件，支持多选单选，自定义排序事件，多种视图
+- 计划
+  * [ ] v1.0 实现拖拽排序的核心功能
+  * [ ] v2.0 实现定制化功能
+
+## Features
+* [ ] 核心拖拽多选
+* [ ] 加载中
+* [ ] 序号
+* [ ] 分组
+* [ ] 筛选
+* [ ] 分页
+* [ ] 双击查看详情
+* [ ] 右键快捷菜单
+* [ ] 键盘事件
+* [ ] 操作栈（可撤销）
+* [ ] alt显示序号
+* [ ] API 设计
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -66,3 +90,7 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+## License
+[Anti-996 License] (LICENSE)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './style.scss';
+import data from './mock';
 
 function App() {
+  console.log('data: ', data.list)
+  const galleryItems = data.list.map((item) =>
+    <li className="gallery-item" key={item.id}>
+      <img src={item.image} alt={item.title}/>
+    </li>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article className="App">
+      <section className="playground">
+        <header>Playground</header>
+        <ul className="gallery-list">
+          {galleryItems}
+        </ul>
+      </section>
+    </article>
   );
 }
 
